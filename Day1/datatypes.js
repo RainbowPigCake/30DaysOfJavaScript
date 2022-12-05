4b4404eb0352c9e07cb44c2774d090bfaf5937e0
RainbowPigCake <d58epj2XGZZC6ri>
2022-12-05 01:41:45 -0500

    day 1

---
+++ Day1/datatypes.js
@@ -0,0 +1,41 @@
+//5
+let s = 'hello everyone'  
+let b = true
+let undef 
+let n = null
+
+
+console.log(typeof s)
+console.log(typeof b)
+console.log(typeof undef)
+console.log(typeof n)
+
+
+//6
+let a1
+let a2
+let a3
+let a4
+
+//7
+let b1 = 5
+let b2 = 'idk'
+let b3 = true
+let b4 = 4.1
+
+//8
+let firstName1 = 'Random'
+let lastName1 = 'Pancake'
+let maritalStatus1 = 'Partner'
+let country1 = 'Canada'
+let age1 = 1
+
+//9
+let firstName2 = 'Random', lastName2 = 'Pancake', maritalStatus2 = 'Partner', country2 = 'Canada', age2 = 1
+console.log('age = ', age2)
+
+//10
+let myAge = 5
+let yourAge = 50
+console.log('I am', myAge, 'year(s) old')
+console.log('You are', yourAge, 'year(s) old')    
---
+++ Day1/variable.js
@@ -0,0 +1,5 @@
+//4
+let str1 = 'hello everyone'
+let b7 = true
+let undef1
+let n1 = null
